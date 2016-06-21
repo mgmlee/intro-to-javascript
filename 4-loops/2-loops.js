fd04a37cdb52d20acf045e24747763ec87e45285
DejonGill <dejonjgill@gmail.com>
2016-06-20 19:44:20 -0700

    Finished comments for objects and arrays

--- 4-loops/2-loops.js
+++ 4-loops/2-loops.js
@@ -1,3 +1,7 @@
 /*
-    
+  Great job! We've been covering a lot of material, super quickly.
+  Take a moment to be prooud of yourself!
+
+  For this section, we need to take a closer look at strings.
+  First, take a moment to familarize yourself with escape characters.  
 */
